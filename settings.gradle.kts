8473be9ab30ac939da06e53262b7e5de738251ca
Alpha <adiallo@exomind.fr>
2024-05-31 10:48:30 +0200

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Free_Wifi_Paris"
+include(":androidApp")
+include(":shared")
