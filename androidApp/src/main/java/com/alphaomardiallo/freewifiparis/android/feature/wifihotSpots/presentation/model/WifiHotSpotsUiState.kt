17f40aafc188e205113f014762b3e994cd72d1f0
Alpha <adiallo@exomind.fr>
2024-06-03 23:14:13 +0200

    feature : safe API call

--- androidApp/src/main/java/com/alphaomardiallo/freewifiparis/android/feature/wifihotSpots/presentation/model/WifiHotSpotsUiState.kt
+++ androidApp/src/main/java/com/alphaomardiallo/freewifiparis/android/feature/wifihotSpots/presentation/model/WifiHotSpotsUiState.kt
@@ -2,9 +2,11 @@ package com.alphaomardiallo.freewifiparis.android.feature.wifihotSpots.presentat
 
 import com.alphaomardiallo.freewifiparis.common.presentation.ErrorInfoUi
 import com.alphaomardiallo.freewifiparis.feature.wifiHotspots.presentation.HotSpotsUi
+import com.alphaomardiallo.freewifiparis.feature.wifiHotspots.presentation.ResultUi
 
 data class WifiHotSpotsUiState(
     val wifiHotSpots: HotSpotsUi = HotSpotsUi(),
+    val hotSpotsList: MutableList<ResultUi> = mutableListOf(),
     val isLoading: Boolean = false,
     val error: ErrorInfoUi = ErrorInfoUi(),
 )
--- androidApp/src/main/java/com/alphaomardiallo/freewifiparis/android/feature/wifihotSpots/presentation/viewmodel/WifiHotSpotsViewModel.kt
+++ androidApp/src/main/java/com/alphaomardiallo/freewifiparis/android/feature/wifihotSpots/presentation/viewmodel/WifiHotSpotsViewModel.kt
@@ -7,6 +7,7 @@ import com.alphaomardiallo.freewifiparis.android.feature.wifihotSpots.presentati
 import com.alphaomardiallo.freewifiparis.common.domain.DomainResponse
 import com.alphaomardiallo.freewifiparis.common.presentation.ErrorInfoUi
 import com.alphaomardiallo.freewifiparis.feature.wifiHotspots.domain.usecase.GetWifiHotspotsUseCase
+import com.alphaomardiallo.freewifiparis.feature.wifiHotspots.presentation.HotSpotsUi
 import io.github.aakira.napier.Napier
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -27,57 +28,100 @@ class WifiHotSpotsViewModel(
 
     fun getWifiHotspots() {
         viewModelScope.launch {
-            getWifiHotspotsUseCase.invoke().collect { response ->
+            _uiState.update { state ->
+                state.copy(
+                    isLoading = true,
+                    hotSpotsList = mutableListOf(),
+                    wifiHotSpots = HotSpotsUi(),
+                    error = ErrorInfoUi(
+                        code = null,
+                        message = null
+                    )
+                )
+            }
 
-                when (response) {
-                    is DomainResponse.Error -> {
-                        _uiState.update { state ->
-                            state.copy(
-                                isLoading = false,
-                                error = ErrorInfoUi(
-                                    code = response.errorCode,
-                                    message = response.description
+            districtList().map { district ->
+                getWifiHotspotsUseCase.invoke(district).collect { response ->
+
+                    when (response) {
+                        is DomainResponse.Error -> {
+                            _uiState.update { state ->
+                                state.copy(
+                                    isLoading = false,
+                                    error = ErrorInfoUi(
+                                        code = response.errorCode,
+                                        message = response.description
+                                    )
                                 )
-                            )
+                            }
+
+                            Napier.e(response.description.toString())
                         }
 
-                        Napier.e(response.description.toString())
-                    }
-
-                    is DomainResponse.Loading -> {
-                        _uiState.update { state ->
-                            state.copy(
-                                isLoading = true,
-                                error = ErrorInfoUi(
-                                    code = null,
-                                    message = null
+                        is DomainResponse.Loading -> {
+                            _uiState.update { state ->
+                                state.copy(
+                                    isLoading = true,
+                                    error = ErrorInfoUi(
+                                        code = null,
+                                        message = null
+                                    )
                                 )
-                            )
+                            }
+
+                            Napier.i("Loading")
                         }
 
-                        Napier.i("Loading")
-                    }
+                        is DomainResponse.Success -> {
+                            val list = uiState.value.hotSpotsList
+                            response.response.toHotSpotsUi().hotSpotsList.map { list.add(it) }
 
-                    is DomainResponse.Success -> {
-                        _uiState.update { state ->
-                            state.copy(
-                                wifiHotSpots = response.response.toHotSpotsUi(),
-                                isLoading = false,
-                                error = ErrorInfoUi(
-                                    code = null,
-                                    message = null
+                            _uiState.update { state ->
+                                state.copy(
+                                    wifiHotSpots = response.response.toHotSpotsUi(),
+                                    hotSpotsList = list,
+                                    isLoading = false,
+                                    error = ErrorInfoUi(
+                                        code = null,
+                                        message = null
+                                    )
                                 )
-                            )
-                        }
+                            }
 
-                        Napier.d(response.toString())
+                            Napier.d(response.toString())
+                        }
                     }
                 }
             }
         }
     }
 
-    fun getMarkers() = _uiState.value.wifiHotSpots.hotSpotsList.map {
+    fun getMarkers() = _uiState.value.hotSpotsList.map {
         toHotSpotsMarkerUiUseCase.execute(it)
     }
+
+    private fun districtList() = listOf(
+        "75001",
+        "75002",
+        "75003",
+        "75004",
+        "75005",
+        "75006",
+        "75007",
+        "75008",
+        "75009",
+        "75010",
+        "75011",
+        "75012",
+        "75013",
+        "75014",
+        "75015",
+        "75016",
+        "75017",
+        "75018",
+        "75019",
+        "75020",
+        "92100",
+        "94380"
+    )
 }
