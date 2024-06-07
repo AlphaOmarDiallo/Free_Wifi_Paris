b8cd0dc5eafc50b000f42bf1448be02435cdaa30
Alpha <adiallo@exomind.fr>
2024-06-07 16:18:39 +0200

    feature : bottom sheet

--- androidApp/src/main/java/com/alphaomardiallo/freewifiparis/android/common/MainActivity.kt
+++ androidApp/src/main/java/com/alphaomardiallo/freewifiparis/android/common/MainActivity.kt
@@ -6,18 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.alphaomardiallo.freewifiparis.android.feature.wifihotSpots.presentation.WifiHotSpotsScreen
-import io.github.aakira.napier.Napier
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
+                val sheetState = rememberModalBottomSheetState()
+                val scope = rememberCoroutineScope()
+                var showBottomSheet by remember { mutableStateOf(false) }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -25,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     Scaffold(
                         topBar = {
                             MainTopAppBar {
-                                Napier.i("Top app bar favAction clicked")
+                                showBottomSheet = true
                             }
                         }
                     ) {
@@ -35,6 +47,17 @@ class MainActivity : ComponentActivity() {
                                 .padding(it)
                         ) {
                             WifiHotSpotsScreen()
+
+                            if (showBottomSheet) {
+                                ModalBottomSheet(
+                                    onDismissRequest = {
+                                        showBottomSheet = false
+                                    },
+                                    sheetState = sheetState
+                                ) {
+
+                                }
+                            }
                         }
                     }
                 }
@@ -42,4 +65,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
